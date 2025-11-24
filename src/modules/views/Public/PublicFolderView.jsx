5c4bdf2cec184d8731d38f4aab8b5ec77c98c8ed
lethemanh <lethemanh@lethemanhs-MacBook-Pro.local>
2025-11-24 14:45:08 +0700

    feat: Implement summarize file by AI :sparkles:

--- src/modules/views/Public/PublicFolderView.jsx
+++ src/modules/views/Public/PublicFolderView.jsx
@@ -44,7 +44,8 @@ import {
   rename,
   versions,
   selectAllItems,
-  hr
+  hr,
+  summariseByAI
 } from '@/modules/actions'
 import { duplicateTo } from '@/modules/actions/components/duplicateTo'
 import { moveTo } from '@/modules/actions/components/moveTo'
@@ -184,6 +185,9 @@ const PublicFolderView = () => {
     [
       selectAllItems,
       download,
+      hr,
+      summariseByAI,
+      hr,
       moveTo,
       duplicateTo,
       hr,
